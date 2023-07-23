c78a164607c27e3ee92804356e64c4482bb0635f
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-07-23 17:44:41 +0300

    TT cutoffs, Increase TT size (#12)
    Tokens: 899

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -10,13 +10,15 @@ public class MyBot : IChessBot
     ulong[] psts = {657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 67159620133902};
 
     struct TTEntry {
-        ulong key;
+        public ulong key;
         public Move move;
-
-        public TTEntry(ulong _key, Move _move) {key = _key; move = _move; }
+        public int depth, score, bound;
+        public TTEntry(ulong _key, Move _move, int _depth, int _score, int _bound) {
+            key = _key; move = _move; depth = _depth; score = _score; bound = _bound;
+        }
     }
 
-    const int entries = (1 << 18);
+    const int entries = (1 << 20);
     ulong nodes = 0;
     TTEntry[] tt = new TTEntry[entries];
 
@@ -60,20 +62,28 @@ public class MyBot : IChessBot
             }
         }
 
+        TTEntry entry = tt[key % entries];
+
+        if(ply > 0 && entry.key == key && entry.depth >= depth && 
+        (entry.bound == 3 || (entry.bound == 2 && entry.score >= beta) || (entry.bound == 1 && entry.score <= alpha)))
+            return entry.score;
+
         if(qsearch) {
             best = Evaluate(board);
             if(best >= beta) return best;
             if(best > alpha) alpha = best;
         }
 
-        TTEntry entry = tt[key % entries];
         Move[] moves = board.GetLegalMoves(qsearch);
         int[] scores = new int[moves.Length];
+
         for(int i = 0; i < moves.Length; i++) {
             if(moves[i] == entry.move) scores[i] = 1000000;
             else if(moves[i].IsCapture) scores[i] = 100 * moves[i].TargetSquare.Index - moves[i].StartSquare.Index;
         }
+
         Move bestMove = Move.NullMove;
+        int origAlpha = alpha;
         for(int i = 0; i < moves.Length; i++) {
             if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return 0;
 
@@ -103,7 +113,7 @@ public class MyBot : IChessBot
             if(board.IsInCheck()) return -30000 + ply;
             else return 0;
         }
-        tt[key % entries] = new TTEntry(key, bestMove);
+        tt[key % entries] = new TTEntry(key, bestMove, depth, best, best >= beta ? 2 : best > origAlpha ? 3 : 1);
         return best;
     }
     public Move Think(Board board, Timer timer)
